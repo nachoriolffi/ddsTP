3268ab800afeaf90aca921a8e74fd4a8f08c24ac
NahuelLazarte <clazarte@frba.utn.edu.ar>
2024-04-19 22:34:58 -0300

    Contacto

---
+++ src/main/java/ar/edu/utn/frba/dds/contacto/CorreoElectronico.java
@@ -0,0 +1,10 @@
+package ar.edu.utn.frba.dds.contacto;
+
+import ar.edu.utn.frba.dds.colaborador.Colaborador;
+
+public class CorreoElectronico extends Mensaje implements MedioDeComunicacion{
+    @Override
+    public void comunicar(Mensaje mensaje, Colaborador destinatario) {
+
+    }
+}
--- src/main/java/ar/edu/utn/frba/dds/contacto/MedioDeComunicacion.java
+++ src/main/java/ar/edu/utn/frba/dds/contacto/MedioDeComunicacion.java
@@ -1,4 +1,7 @@
 package ar.edu.utn.frba.dds.contacto;
 
-public interface MedioDeComunicacion {
+import ar.edu.utn.frba.dds.colaborador.Colaborador;
+
+public interface MedioDeComunicacion{
+    public void comunicar (Mensaje mensaje, Colaborador destinatario);
 }
