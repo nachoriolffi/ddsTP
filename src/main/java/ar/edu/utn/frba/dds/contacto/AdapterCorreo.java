4c75fb1b45f5acb5db1218ee0b1f1a5dd2728f0b
federico perez <federperez@frba.utn.edu.ar>
2024-04-21 19:28:27 -0300

    agrego los adapter

---
+++ src/main/java/ar/edu/utn/frba/dds/contacto/AdapterCorreo.java
@@ -0,0 +1,7 @@
+package ar.edu.utn.frba.dds.contacto;
+
+import ar.edu.utn.frba.dds.colaborador.Colaborador;
+
+public class AdapterCorreo {
+    public void comunicarMensaje(Mensaje mensaje, Colaborador destinatario){}
+}
---
+++ src/main/java/ar/edu/utn/frba/dds/contacto/AdapterTelefono.java
@@ -0,0 +1,8 @@
+package ar.edu.utn.frba.dds.contacto;
+
+import ar.edu.utn.frba.dds.colaborador.Colaborador;
+
+public class AdapterTelefono {
+
+    public void comunicarMensaje(Mensaje mensaje, Colaborador destinatario){}
+}
