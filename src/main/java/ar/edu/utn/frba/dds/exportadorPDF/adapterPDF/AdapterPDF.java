2306905b49139a042865e710a0d9a8f47c02a583
federico perez <federperez@frba.utn.edu.ar>
2024-06-16 19:35:35 -0300

    mas cosas de exportador pdf pero sigue sin hacer el salto para la escritura de las tablas mama matame

--- src/main/java/ar/edu/utn/frba/dds/exportadorPDF/adapterPDF/AdapterPDF.java
+++ src/main/java/ar/edu/utn/frba/dds/exportadorPDF/adapterPDF/AdapterPDF.java
@@ -7,39 +7,48 @@ import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
-import com.itextpdf.layout.property.UnitValue;
+import com.itextpdf.layout.properties.UnitValue;
+import java.io.File;
 
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.HashMap;
-
 import java.util.List;
 import java.util.Map;
 
 public class AdapterPDF implements InterfaceAdapterPDF{
 
+
+        // para esto necesitan tener un archivo pdf en el escritorio para poder hacerlo andar
+    // caso contrario no van a poder hacerlo andar
         private String pathDocumento="C:\\Users\\user\\Desktop\\exportar.PDF";
 
-    public static void exportToPdf(String dest, List<Exportable> exportables) throws FileNotFoundException {
+    public void exportToPdf(String dest, List<Exportable> exportables) throws FileNotFoundException {
+        File file = new File(dest);
+        if (!file.getParentFile().exists()) {
+            file.getParentFile().mkdirs(); // Crea el directorio si no existe
+        }
 
         PdfWriter writer = new PdfWriter(dest);
         PdfDocument pdf = new PdfDocument(writer);
         Document document = new Document(pdf);
 
         // Para cada sección de datos, añade un título y una tabla al documento
-        for (int i = 0; i < exportables.size(); i++) {
-            document.add(new Paragraph("Sección " + (i + 1)));
-            addTableToDocument(exportables.get(i).datos(), document);
+        for (Exportable exportable : exportables) {
+            Map<String, List<String>> datos = exportable.datos();
+
+            document.add(new Paragraph("Sección"));
+            addTableToDocument(datos, document);
+            document.getPdfDocument().addNewPage(); // Agrega una nueva página para cada exportable
         }
 
-        document.close();
+        document.close(); // Cierra el documento al finalizar todas las iteraciones
     }
-
     public static void addTableToDocument(Map<String, List<String>> data, Document document) {
         // Crear una tabla con tantas columnas como llaves en el mapa
-        if(data == null){
-            System.out.println("No hay nada para agregar al pdf");
+        if (data == null || data.isEmpty()) {
+            return; // No hacer nada si los datos son nulos o vacíos
         }
+
+        // Crear una tabla con tantas columnas como llaves en el mapa
         Table table = new Table(UnitValue.createPercentArray(data.keySet().size()));
         table.setWidth(UnitValue.createPercentValue(100));
 
@@ -56,10 +65,14 @@ public class AdapterPDF implements InterfaceAdapterPDF{
             for (String key : data.keySet()) {
                 table.addCell(data.get(key).get(i));
             }
+            document.add(table); // Agregar la tabla al documento en cada iteración para que cada tabla se agregue en una página separada
+            table = new Table(UnitValue.createPercentArray(data.keySet().size())); // Crear una nueva tabla para la siguiente página
+            table.setWidth(UnitValue.createPercentValue(100)); // Establecer el ancho de la nueva tabla
+            // Añadir encabezados para la nueva tabla
+            for (String key : data.keySet()) {
+                table.addHeaderCell(key);
+            }
         }
-
-        // Añadir la tabla al documento
-        document.add(table);
     }
 
 
--- src/main/java/ar/edu/utn/frba/dds/tarjeta/Tarjeta.java
+++ src/main/java/ar/edu/utn/frba/dds/tarjeta/Tarjeta.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Getter
 @Setter
 public class Tarjeta {
+    
     private Integer idTarjeta;
     private List<UsoTarjeta> registroUsos;
     private Vulnerable personaAsociada;
