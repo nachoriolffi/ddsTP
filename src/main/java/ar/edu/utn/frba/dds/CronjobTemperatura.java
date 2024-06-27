9317980c11ef59f189dd5999a5ad1f74fa81f50a
NahuelLazarte <clazarte@frba.utn.edu.ar>
2024-06-27 17:00:28 -0300

    Funciones para CronjobTemperatura

--- src/main/java/ar/edu/utn/frba/dds/CronjobTemperatura.java
+++ src/main/java/ar/edu/utn/frba/dds/CronjobTemperatura.java
@@ -8,10 +8,14 @@ import ar.edu.utn.frba.dds.models.entities.heladera.receptor.ReceptorTemperatura
 import ar.edu.utn.frba.dds.models.entities.ubicacionGeografica.Coordenada;
 import ar.edu.utn.frba.dds.models.entities.ubicacionGeografica.Direccion;
 import ar.edu.utn.frba.dds.models.entities.vianda.Vianda;
+import ar.edu.utn.frba.dds.models.repositories.implementaciones.RepoHeladeras;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class CronjobTemperatura {
     public static void main(String[] args) {
@@ -34,22 +38,50 @@ public class CronjobTemperatura {
 
         heladera.setReceptorTemperatura(receptorTemp);
 
+        RepoHeladeras repoHeladeras = RepoHeladeras.getInstancia();
+        repoHeladeras.agregarHeladera(heladera);
+        List<Heladera> todasLasHeladeras = repoHeladeras.traerHeladeras();
+
+        controlarUltimasLecturasHeladeras(todasLasHeladeras, 0);
+
+    }
+    static void controlarUltimaLectura(Heladera heladera, long tiempoEnMinutos){
         List<RegistroTemperatura> temperaturas = new ArrayList<>(heladera.getReceptorTemperatura().getTemperaturasLeidas());
-        RegistroTemperatura ultimaLectura = temperaturas.get(temperaturas.size() - 1);
+        RegistroTemperatura ultimaLectura = null;
+        if (!temperaturas.isEmpty()) {
+            ultimaLectura = temperaturas.get(temperaturas.size() - 1);
 
-        SimpleDateFormat formato = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-        String fechaFormateada = formato.format(ultimaLectura.getFechaHora());
+            Date fechaUltimaLectura = ultimaLectura.getFechaHora();
+            Date fechaActual = new Date();
 
-        System.out.println("La temperatura leida es: " + ultimaLectura.getLectura() + " y la fecha y hora es: " + fechaFormateada);
+            long diferenciaMilisegundos = Math.abs(fechaActual.getTime() - fechaUltimaLectura.getTime());
+            long diferenciaMinutos = TimeUnit.MINUTES.convert(diferenciaMilisegundos, TimeUnit.MILLISECONDS);
 
-        //assert ultimaLectura.getLectura() == 18;
-        // crear receptor de temperatura, crear modelo
+            if (diferenciaMinutos > tiempoEnMinutos) {
+                System.out.println("La diferencia entre la hora de la última lectura y la hora actual es mayor a " + tiempoEnMinutos + " minutos");
+                heladera.setEstaActiva(false);
+                /*
+                   reportar el incidente indicando la fecha y hora del mismo, en qué heladera ocurrió
+                   y el tipo. En caso de que sea una alerta indicar cuál fue.
+                */
+            }
+            else {
+                System.out.println("La diferencia entre la hora de la última lectura y la hora actual no es mayor a " + tiempoEnMinutos + " minutos");
+            }
 
+            SimpleDateFormat formato = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+            String fechaFormateada = formato.format(ultimaLectura.getFechaHora());
+            System.out.println("La temperatura leida es: " + ultimaLectura.getLectura() + " y la fecha y hora es: " + fechaFormateada);
+        }
+        else {
+            System.out.println("La lista de temperaturas está vacía.");
+        }
+    }
+
+    static void controlarUltimasLecturasHeladeras(List<Heladera> heladeras, long tiempoEnMinutos) {
+        for (Heladera heladera : heladeras) {
+            controlarUltimaLectura(heladera, tiempoEnMinutos);
+        }
     }
 
 }
-/*
-Tengo una lista con varias heladeras y dentro está el sensor y dentro
-receptorTemperatura. Dentro del receptor está la lista de temperaturas leídas.
-Lo que importa es el horario. es decir, obtener un elemento y controlar el horario.
-*/
