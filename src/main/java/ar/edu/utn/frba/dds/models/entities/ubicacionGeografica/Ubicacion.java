b4fc3d98d4f6332ddb44c10a77ad76012fbbb118
federico perez <federperez@frba.utn.edu.ar>
2024-09-04 02:07:38 -0300

    coass no se tengo sueño

--- src/main/java/ar/edu/utn/frba/dds/models/entities/ubicacionGeografica/Ubicacion.java
+++ src/main/java/ar/edu/utn/frba/dds/models/entities/ubicacionGeografica/Ubicacion.java
@@ -1,34 +1,33 @@
 package ar.edu.utn.frba.dds.models.entities.ubicacionGeografica;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
-@Entity
-@Table(name = "ubicacion")
+@Getter
+@Setter
+@Embeddable
 public class Ubicacion {
 
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id_Ubicacion;
-
-    @ManyToOne (cascade = CascadeType.ALL)
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "id_Provincia", referencedColumnName = "id_Provincia")
     private Provincia provincia;
 
-    @ManyToOne (cascade = CascadeType.ALL)
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "id_Municipio", referencedColumnName = "id_Municipio")
     private Municipio municipio;
 
-    @ManyToOne (cascade = CascadeType.ALL)
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "id_Localidad", referencedColumnName = "id_Localidad")
     private Localidad localidad;
-    //@ManyToOne
-    //private Coordenada coordenada;
 
     public Ubicacion(Provincia provincia, Municipio municipio, Localidad localidad) {
         this.provincia = provincia;
         this.municipio = municipio;
         this.localidad = localidad;
+    }
 
+    public Ubicacion() {
     }
 }
---
+++ src/test/java/ar/edu/utn/frba/dds/Persistencia/UbicacionPersistencia.java
@@ -0,0 +1,5 @@
+package ar.edu.utn.frba.dds.Persistencia;
+
+public class UbicacionPersistencia {
+
+}
