29e4a294dc59da8e7045b299a39bfe497582033b
NahuelLazarte <clazarte@frba.utn.edu.ar>
2024-10-11 00:12:27 -0300

    Avances en distribuirViandas. Agrego checkbox de whatsapp y telegram en registroJuridico

--- src/main/java/ar/edu/utn/frba/dds/controllers/DistribuirViandasController.java
+++ src/main/java/ar/edu/utn/frba/dds/controllers/DistribuirViandasController.java
@@ -3,13 +3,14 @@ package ar.edu.utn.frba.dds.controllers;
 import ar.edu.utn.frba.dds.models.entities.colaborador.formasColab.DistribucionVianda;
 import ar.edu.utn.frba.dds.models.entities.colaborador.formasColab.MotivoDistribucion;
 import ar.edu.utn.frba.dds.models.entities.heladera.Heladera;
+import ar.edu.utn.frba.dds.models.repositories.implementaciones.RepoHeladeras;
 import ar.edu.utn.frba.dds.utils.ICrudViewsHandler;
 import io.javalin.http.Context;
 
 import java.sql.Date;
 import java.text.ParseException;
 
-public class DistribuirViandasController  extends BaseController implements ICrudViewsHandler {
+public class DistribuirViandasController implements ICrudViewsHandler {
     @Override
     public void index(Context context) {
         context.render("donaciones/distribuirViandas.hbs");
@@ -33,11 +34,14 @@ public class DistribuirViandasController  extends BaseController implements ICru
         System.out.println("cantidad-viandas: " + context.formParam("cantidad-viandas"));
         System.out.println("fecha-distribucion: " + context.formParam("fecha-distribucion"));
 
-        Heladera heladeraOrigen = new Heladera();//lo estoy harcodeando, pero deberia ser un getHeladeraByNombre
-        Heladera heladeraDestino = new Heladera();
-        //context.formParam("heladera-origen");
-        //context.formParam("heladera-destino");
+        //Esto es para la persona humana, así que hay que chequear que tenga ese rol
+        //también se podría asumir que cada heladera tiene un nombre unico, entonces la recuperamos por el nombre
 
+        Long heladeraOrigen_id = Long.valueOf(context.formParam("heladera-origen"));
+        Long heladeraDestino_id = Long.valueOf(context.formParam("heladera-destino"));
+
+        Heladera heladeraOrigen =  RepoHeladeras.INSTANCE.buscar(heladeraOrigen_id);
+        Heladera heladeraDestino =  RepoHeladeras.INSTANCE.buscar(heladeraDestino_id);
 
         DistribucionVianda distribucionVianda = new DistribucionVianda();
         distribucionVianda.setHeladeraOrigen(heladeraOrigen);
@@ -52,6 +56,8 @@ public class DistribuirViandasController  extends BaseController implements ICru
         distribucionVianda.setFechaDistribucion(Date.valueOf(context.formParam("fecha-distribucion")));//probar
         distribucionVianda.setFechaColaboracion(new java.util.Date());
 
+        //Habría que quitar las viandas de la heladera A segun la cantidad y agregarlas a la heladera B
+        //Despues hay que hacer un update o modificar
     }
 
     @Override
