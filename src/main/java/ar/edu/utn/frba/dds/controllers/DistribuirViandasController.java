fc97676a51cd3057972ac74eb92e1c9f26185e9d
NahuelLazarte <clazarte@frba.utn.edu.ar>
2024-10-11 10:59:25 -0300

    Ahora distribuirViandas persiste en la base de datos y también funcionan los checkbox de regitroJuridico

--- src/main/java/ar/edu/utn/frba/dds/controllers/DistribuirViandasController.java
+++ src/main/java/ar/edu/utn/frba/dds/controllers/DistribuirViandasController.java
@@ -3,6 +3,7 @@ package ar.edu.utn.frba.dds.controllers;
 import ar.edu.utn.frba.dds.models.entities.colaborador.formasColab.DistribucionVianda;
 import ar.edu.utn.frba.dds.models.entities.colaborador.formasColab.MotivoDistribucion;
 import ar.edu.utn.frba.dds.models.entities.heladera.Heladera;
+import ar.edu.utn.frba.dds.models.repositories.implementaciones.RepoDistribucionVianda;
 import ar.edu.utn.frba.dds.models.repositories.implementaciones.RepoHeladeras;
 import ar.edu.utn.frba.dds.utils.ICrudViewsHandler;
 import io.javalin.http.Context;
@@ -36,6 +37,10 @@ public class DistribuirViandasController implements ICrudViewsHandler {
 
         //Esto es para la persona humana, así que hay que chequear que tenga ese rol
         //también se podría asumir que cada heladera tiene un nombre unico, entonces la recuperamos por el nombre
+        //en este caso solo se registra la contribución, no se hace la disribución
+
+        //fechaDistribucion -> es la fecha en la que se van a distribuir las viandas
+        //fechaColaboracion -> es la fecha en la que colaborador completo el formulario
 
         Long heladeraOrigen_id = Long.valueOf(context.formParam("heladera-origen"));
         Long heladeraDestino_id = Long.valueOf(context.formParam("heladera-destino"));
@@ -56,6 +61,9 @@ public class DistribuirViandasController implements ICrudViewsHandler {
         distribucionVianda.setFechaDistribucion(Date.valueOf(context.formParam("fecha-distribucion")));//probar
         distribucionVianda.setFechaColaboracion(new java.util.Date());
 
+        RepoDistribucionVianda.INSTANCE.agregar(distribucionVianda);
+
+        //Lo siguiente se haría cuando el colaborador asista personalmente a realizar la distribución:
         //Habría que quitar las viandas de la heladera A segun la cantidad y agregarlas a la heladera B
         //Despues hay que hacer un update o modificar
     }
